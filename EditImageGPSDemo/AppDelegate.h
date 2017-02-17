eb35c7886ee0bc71118be5f2e6ac21eb62a514e1
lihongli <honglili@sohu-inc.com>
2017-02-17 15:17:46 +0800

    Initial Commit

---
+++ EditImageGPSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EditImageGPSDemo
+//
+//  Created by LHL on 17/2/17.
+//  Copyright © 2017年 lihongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EditImageGPSDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EditImageGPSDemo
+//
+//  Created by LHL on 17/2/17.
+//  Copyright © 2017年 lihongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
